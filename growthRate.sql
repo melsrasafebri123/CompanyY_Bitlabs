179d2a1128142ab125e361e9b5394494dd5b506c
Melsra Safebri <80032143+melsrasafebri123@users.noreply.github.com>
2023-06-02 21:18:06 +0700

    Create growthRate.sql

---
+++ growthRate.sql
@@ -0,0 +1,16 @@
+SELECT 
+  ((SELECT
+      COUNT(employeeID)
+    FROM
+      `I_CID_05.employees_2011`
+    WHERE
+      status = 'ACTIVE'
+      AND recorddate_key <= '2011-12-31')
+  /
+  (SELECT
+    COUNT(employeeID)
+  FROM
+    `I_CID_05.employees_2010`
+  WHERE
+    status = 'ACTIVE'
+    AND recorddate_key >= '2010-01-01')) AS GR1011;
